749e5a33742afcb1859a6a165774ab04ff1f44b4
Zhu-Guojun <2585104613@qq.com>
2026-09-16 09:54:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // 把这里改成你自己的 Supabase 项目信息，然后将本文件另存为 config.js。
 // Supabase 后台：Project Settings → API
 window.APP_CONFIG = {
-  supabaseUrl: 'https://oyavtrmhcubcsnhuepvy.supabase.co/rest/v1/',
-  supabaseAnonKey: 'oyavtrmhcubcsnhuepvy'
+  supabaseUrl: 'https://oyavtrmhcubcsnhuepvy.supabase.co',
+  supabaseAnonKey: 'sb_publishable_cvU9ZKKURkMUrtH4WmxMug_qH-3IfE9'
 };
